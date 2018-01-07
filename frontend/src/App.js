c09351af29f46dc146ef65366972aa55aff406c8
J1marotta <jim_is55@hotmail.com>
2018-01-07 15:24:35 +1100

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Checkout from './Checkout';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <Checkout
+            name={'James Made This'}
+            description={'Really!'}
+            amount={1}
+          />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
